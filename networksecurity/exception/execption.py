77386541b4f47c8668d8e73973880ac98b53a8a4
chitaki10 <chitakin876@gmail.com>
2025-09-08 17:50:59 +0530

    logging and execption

--- networksecurity/exception/execption.py
+++ networksecurity/exception/execption.py
@@ -1,31 +1,24 @@
 import sys
+from networksecurity.logging import logger
 
 # Custom exception class
 class NetworkSecurityException(Exception):
-    def __init__(self, error_message, error_details: sys):
-        """
-        Constructor (__init__) is called automatically when creating the object.
-        It captures the error message, file name, and line number.
-        """
-        super().__init__(error_message)              # Pass message to base Exception class
+    def __init__(self, error_message):
+        super().__init__(error_message)
         self.error_message = error_message
-
-        # Extract traceback details from sys.exc_info()
-        _, _, exc_tb = error_details.exc_info() #details.exc_info() returns a tuple (type, value, traceback)
-        self.lineno = exc_tb.tb_lineno#gives the line number where the exception occurred
-        self.file_name = exc_tb.tb_frame.f_code.co_filename# gives the file name where the exception occurred
+        # Extract traceback details
+        _, _, exc_tb = sys.exc_info()
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        """
-        This method defines what should be printed when the exception is converted to a string.
-        """
         return f"Error occurred in script [{self.file_name}] at line [{self.lineno}]: {self.error_message}"
 
 
 if __name__ == "__main__":
     try:
-        a = 1 / 0                       # Force an error
+        logger.logging.info("Enter the try block")
+        a = 1 / 0
         print("This will not be printed", a)
     except Exception as e:
-        # Raise the custom exception with the original error details
-        raise NetworkSecurityException(str(e), sys)
+        raise NetworkSecurityException(str(e))
--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -1,16 +1,23 @@
-import logging 
+import logging
 import os
 from datetime import datetime
 
+# Build log file name
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok = True)
+# Make sure the "logs" directory exists
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+# Full path to the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
+# Configure the logging system
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO, 
+    level=logging.INFO,
 )
+
+# Create and export a logger object
+logger = logging.getLogger(__name__)
